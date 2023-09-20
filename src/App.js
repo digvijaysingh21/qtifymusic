3d892a63c66068c8b8618754fbe200ad06a5d585
digvijaysingh21 <singh.v.digvijay@gmail.com>
2023-09-20 15:29:24 +0530

    cardCarouselNewAlbums-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { fetchTopAlbums } from './api/api';
+import { fetchTopAlbums, fetchNewAlbums } from './api/api';
 import NavBar from './components/navbar/navbar';
 import Hero from './components/Hero/hero';
 import Card from './components/Card/card';
@@ -11,6 +11,8 @@ import Section from './components/Section/section';
 function App() {
 
   const [topAlbumData, setTopAlbumData] = useState([])
+
+  const[newAlbumData, setNewAlbumData] = useState([]);
 
   const generateData = async () =>{
     try{
@@ -23,8 +25,18 @@ function App() {
     
   }
 
+  const generateNewAlbumData = async () =>{
+    try{
+      const data = await fetchNewAlbums();
+      setNewAlbumData(data);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   useEffect(() =>{
     generateData();
+    generateNewAlbumData();
   },[])
 
   return (
@@ -36,9 +48,15 @@ function App() {
           <Card key={item.id} data={item} type="album"/>
         )
       })} */}
+      <div className="sectionWrapper">
       <div>
       <Section data={topAlbumData} title={"Top Albums"}/>
       </div>
+      <div>
+      <Section data={newAlbumData} title={"New Albums"}/>
+      </div>
+      </div>
+      <hr className="line"></hr>
     </div>
   );
 }
